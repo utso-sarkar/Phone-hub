1e3d3b79f975282c3d676c1ee83d01eb17f0e0ee
Utso-Sarkar <devutso428@gmail.com>
2023-11-12 03:43:02 +0600

    added git part

--- script.js
+++ script.js
@@ -6,11 +6,34 @@ displayPhones(phones)
 }
 
 const displayPhones= phones=>{
-    phones.forEach(phone=>{
+    // step: 1 :: dom create
+    const phoneContainer=document.getElementById('phone-container')
+    phones.forEach(phone => {
+        
         console.log(phone);
+        // step-2 :: create a div
+        const phoneCard =document.createElement('div');
+        phoneCard.classList= `
+        card card-compact w-96 bg-gray-100 shadow-xl `;
+        // step-3 :: create innerHTML
+        phoneCard.innerHTML=`
+        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div> 
+            </div>
+          `
+          ;
+        //   // step-4 :: append child
+        phoneContainer.appendChild(phoneCard);
     })
 
 }
+
+
 
 
 
