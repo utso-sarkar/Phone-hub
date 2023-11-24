6c95b6ca4d8aa36b7abef5f51ff34ede442a91a2
Utso-Sarkar <devutso428@gmail.com>
2023-11-24 16:17:01 +0600

    added the git part

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const loadPhone = async ("searchText") => {
-    const res=await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchphn}`);
+const loadPhone = async (searchText) => {
+    const res=await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
 const data = await res.json();
 const phones= data.data;
 displayPhones(phones);
@@ -8,6 +8,11 @@ displayPhones(phones);
 const displayPhones= phones=>{
     // step: 1 :: dom create
     const phoneContainer=document.getElementById('phone-container')
+    // to clear phone container 
+    phoneContainer.textContent=''
+
+
+
     phones.forEach(phone => {
         
         console.log(phone);
@@ -19,29 +24,40 @@ const displayPhones= phones=>{
         phoneCard.innerHTML=`
         <figure><img src="${phone.image}" /></figure>
         <div class="card-body">
-          <h2 class="card-title">${phone.phone_name}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+          <h2 class="card-title justify-center">${phone.phone_name}</h2>
+          <p class="text-center">If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-center">
+            <button class="btn btn-primary">Details</button>
           </div> 
             </div>
           `
           ;
         //   // step-4 :: append child
         phoneContainer.appendChild(phoneCard);
-    })
-
+    })  
+    toggleSpinner(false)
 }
+
+
 // search handaler
 
 const handleSearch = () =>{
+  toggleSpinner(true);
   const searchField=document.getElementById("search-field");
   const searchText=searchField.value;
   console.log(searchText);
-  loadPhone(searchField);
+  loadPhone(searchText);
 }
 
-
+const toggleSpinner=(isLoading)=>{
+  const loadingTgl = document.getElementById("loading-spinner");
+  if (isLoading){
+    loadingTgl.classList.remove("hidden")
+  }
+  else{
+    loadingTgl.classList.add("hidden");
+  }
+}
 
 
 
