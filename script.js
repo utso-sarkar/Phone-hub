4147f8f82b46993354171d0b25dbbfed19773411
Utso-Sarkar <devutso428@gmail.com>
2023-12-12 01:18:18 +0600

    added the git part

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const displayPhones= phones=>{
 
     phones.forEach(phone => {
         
-        console.log(phone);
+        // console.log(phone);
         // step-2 :: create a div
         const phoneCard =document.createElement('div');
         phoneCard.classList= `
@@ -23,11 +23,11 @@ const displayPhones= phones=>{
         // step-3 :: create innerHTML
         phoneCard.innerHTML=`
         <figure><img src="${phone.image}" /></figure>
-        <div class="card-body">
+        <div class="card-body ">
           <h2 class="card-title justify-center">${phone.phone_name}</h2>
           <p class="text-center">If a dog chews shoes whose shoes does he choose?</p>
           <div class="card-actions justify-center">
-            <button class="btn btn-primary">Details</button>
+            <button onclick="showDetails('${phone.slug}')" class="btn btn-primary">Details</button>
           </div> 
             </div>
           `
@@ -39,15 +39,65 @@ const displayPhones= phones=>{
 }
 
 
-// search handaler
+// search handalar
 
 const handleSearch = () =>{
   toggleSpinner(true);
   const searchField=document.getElementById("search-field");
   const searchText=searchField.value;
-  console.log(searchText);
+  // console.log(searchText);
   loadPhone(searchText);
 }
+
+
+
+
+
+
+
+
+// modal section create 
+const showDetails = async(id)=>{
+  console.log("Show details",id);
+  const res= await fetch(`https://openapi.programming-hero.com/api/phone/${id}
+  `)
+  const data = await res.json();
+  const phone=data.data;
+  phone_details(phone)
+
+}
+// show the modal 
+const phone_details =(phone)=>{
+  console.log(phone);
+  // const phoneName= document.getElementById("phone-name");
+  // phoneName.innerText=phone.name;
+  const modalContainerInfo=document.getElementById('modal-container-information');
+  modalContainerInfo.innerHTML=`
+    <p><span>Phone Model:</span>${phone.name}</p>
+    <img src ="${phone.image}"/>
+    <p class="text-xl"><span>Phone Storage:</span>${phone.mainFeatures.storage}</p>
+    <p class="text-xl"><span>Phone Display :</span>${phone.mainFeatures.displaySize}</p>
+    <p class="text-xl"><span>Phone Release Date:</span>${phone.releaseDate}</p>
+    <p class="text-xl"><span>USB:</span>${phone.others.USB}</p>
+    <p class="text-xl"><span>Wifi:</span>${phone.others.WLAN}</p>
+    <p class="text-xl"><span>Brand:</span>${phone.brand}</p>
+
+  
+  `
+  
+  
+
+
+
+  
+
+
+
+  // show modal 
+  my_modal_5.showModal();
+}
+
+
 
 const toggleSpinner=(isLoading)=>{
   const loadingTgl = document.getElementById("loading-spinner");
